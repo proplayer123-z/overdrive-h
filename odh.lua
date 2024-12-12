322a063fc95fd0e727ed44af4f8a629234ca9944
proplayer123-z <idkfrcap@gmail.com>
2024-12-12 10:54:02 -0800

    Update odh.lua

--- odh.lua
+++ odh.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://darkscripts.space/scripts/917602878913404939.lua',true))()
+loadstring(game:HttpGet('https://darkscripts.space/scripts/b402bf0b-7812-4ed2-9269-e9876a4da7cb_917602878913404939.lua',true))()
